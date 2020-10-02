ec64b0c671da0a3fc8c25b2996d8d942b6ebf9e7
Keith <kdhuddle@ksu.edu>
2020-10-02 13:24:51 -0500

    Worked on HW3

--- HW3/Breit_Wigner.py
+++ HW3/Breit_Wigner.py
@@ -8,23 +8,23 @@
 # Module Imports =============================================================
 import numpy as np
 import matplotlib.pyplot as plt
-plt.rcParams.update({'font.size': 18})
 
 # Define Parameters Used in Breit-Wigner Formula =============================
 sigma_p = 11.2934  # potential cross section (in barns)
+A = 238.0  # Mass Number
 I = 0  # s wave
-E0, J, GN, GG, _, _ = np.loadtxt('u238.txt', unpack=True)
-G = GN+GG
-A = 238.0
-E = np.logspace(-1, 3, 10000)
-sigma_g = np.zeros_like(E)
-sigma_n = np.zeros_like(E)
-sigma_n[:] = sigma_p
 
-sigma_e = np.zeros_like(E)
+# Load in resonance parameters
+E0, J, GN, GG, GFA, GFB = np.loadtxt('u238.txt', unpack=True)
+G = GN + GG
+# Define Energy Mesh to reconstruct cross section data
+E = np.linspace(0.1, 1000, 50000)
+
+sigma_g = np.zeros_like(E)  # Neutron Gamma interaction (Radiative Capture) 
+sigma_e = np.zeros_like(E)  # Elastic Scatter
 
 #  Nuclear Radius in (cm)
-R = 1.25*10**-13 * 238**(1/3)
+R = 1.25*10**-13 * 238**(1/3) # Nuclear radius approximation
 
 for i in range(len(E0)):
     # statistical spin factor, where I=nuclear spint, J=total spin
@@ -39,24 +39,17 @@ for i in range(len(E0)):
     sigma_e += sigma_0 * (GN[i]/G[i]) * np.sqrt(E0[i]/E) * (1/(1+y**2)) \
              + sigma_0 * (2*R/(4.55*10**-10/E0[i]**0.5))*(y/(1+y**2))
 
-sigma_e = sigma_e + sigma_n  # Add potential scattering
-
-# E2 = tabulated energy values coresponding to exs, elastic cross section
-E2, exs = np.loadtxt('u238e.txt', skiprows=1, delimiter=',', unpack=True)
-E3, rcxs = np.loadtxt('u238rc.txt', skiprows=1, delimiter=',', unpack=True)
+sigma_e = sigma_e + sigma_p  # Add potential scattering
 
 # Plotting ===================================================================
 fig, ((ax1), (ax2)) = plt.subplots(nrows=2, ncols=1, figsize=(12,12))
 ax1.loglog(E, sigma_g, 'orange')
-ax1.loglog(E3, rcxs, 'purple'), ax1.set_xlim(0.1, 1000)
 ax1.set_xlabel('Neutron Energy (eV)')
 ax1.set_ylabel('Cross Section (Barns)')
-ax1.legend(['$Breit-Wigner\ \sigma_\gamma$', '$BNL\ \sigma_\gamma$'], loc=3), plt.tight_layout()
-ax1.grid()
+ax1.legend(['$Breit-Wigner\ \sigma_\gamma$'], loc=3), plt.tight_layout()
 ax2.loglog(E, sigma_e, 'red')
-ax2.loglog(E2, exs, 'green'), ax2.set_xlim(0.1, 1000)
+ax2.set_xlim(0.1, 1000)
 ax2.set_xlabel('Neutron Energy (eV)')
 ax2.set_ylabel('Cross Section (Barns)')
-ax2.legend(['$Breit-Wigner\ \sigma_e$', '$BNL\ \sigma_e$'], loc=3), plt.tight_layout()
-ax2.grid()
+ax2.legend(['$Breit-Wigner\ \sigma_e$'], loc=3), plt.tight_layout()
 plt.show()
--- Neutronics/hw2/Process Data.py
+++ Neutronics/hw2/Process Data.py
@@ -26,71 +26,50 @@ plt.xlabel('Energy [eV]'), plt.ylabel('$\sigma(E) [Barns]$')
 plt.show()
 
 # ============================================================================
-def alpha(A):
-    return ((A-1)/(A+1))**2
-
-def scatter_probability(E_i, E_j, alpha) :
-    p = (1.0/E_j/(1.0-alpha)) * 1.0*((E_i >= alpha*E_j))
-    return p 
-
-
-def compute_spectrum(E, Sigma_t, Sigma_s, Nk, alpha) :
-    N_E = len(E)
-    phi = np.zeros(N_E)
-    phi[N_E-1] = 1.0
-    for i in range(N_E-2, -1, -1) :
-        Q_i = 0.0
-        for j in range(N_E-1, i, -1) :
-            dE = E[j] - E[j-1]
-            E_bar = np.sqrt(E[j]*E[j-1])
-            for k in range(len(Nk)):
-                Q_i += phi[j] * (Sigma_s[k][j]*Nk[k]) * scatter_probability(E[i], E[j], alpha[k]) * dE
-        phi[i] = Q_i / Sigma_t[i]
-        #print (i, phi[i])
-    return phi
-
-# Functions ==================================================================
-def phinr(sd, st, E):
-    return 1 / (E*(st+sd))
-
-def phiwr(sd, sa, E):
-    return 1 / (E*(sa+sd))
-
-sd = 100*H1_es
-fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(12,8))
-ax.loglog(Emesh, phinr(sd,U238_tot,Emesh)/phinr(sd,U238_tot,Emesh)[-1])
-ax.loglog(Emesh, phiwr(sd,U238_ng,Emesh)/phiwr(sd,U238_ng,Emesh)[-1])
-ax.loglog(Emesh, 1/Emesh*Emesh[-1])
-plt.xlabel('Energy [eV]'), plt.ylabel('$\phi(E)$')
-
-
-Nk = [100.0, 1.0]
-Ak = [alpha(1), alpha(238)]
-St = 1.0*(U238_tot)+100.0*H1_es
-Ss = [H1_es, U238_es]
-
-phi = compute_spectrum(Emesh, St, Ss, Nk, Ak)  
-ax.loglog(Emesh, phi/phi[0]*max(1/Emesh*Emesh[-1]))
-plt.legend(['$\phi_{nr}$','$\phi_{wr}$','1/E', 'Slowing Down $\phi$'])
-plt.tight_layout()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#def alpha(A):
+#    return ((A-1)/(A+1))**2
+#
+#def scatter_probability(E_i, E_j, alpha) :
+#    p = (1.0/E_j/(1.0-alpha)) * 1.0*((E_i >= alpha*E_j))
+#    return p 
+#
+#
+#def compute_spectrum(E, Sigma_t, Sigma_s, Nk, alpha) :
+#    N_E = len(E)
+#    phi = np.zeros(N_E)
+#    phi[N_E-1] = 1.0
+#    for i in range(N_E-2, -1, -1) :
+#        Q_i = 0.0
+#        for j in range(N_E-1, i, -1) :
+#            dE = E[j] - E[j-1]
+#            E_bar = np.sqrt(E[j]*E[j-1])
+#            for k in range(len(Nk)):
+#                Q_i += phi[j] * (Sigma_s[k][j]*Nk[k]) * scatter_probability(E[i], E[j], alpha[k]) * dE
+#        phi[i] = Q_i / Sigma_t[i]
+#        #print (i, phi[i])
+#    return phi
+#
+## Functions ==================================================================
+#def phinr(sd, st, E):
+#    return 1 / (E*(st+sd))
+#
+#def phiwr(sd, sa, E):
+#    return 1 / (E*(sa+sd))
+#
+#sd = 100*H1_es
+#fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(12,8))
+#ax.loglog(Emesh, phinr(sd,U238_tot,Emesh)/phinr(sd,U238_tot,Emesh)[-1])
+#ax.loglog(Emesh, phiwr(sd,U238_ng,Emesh)/phiwr(sd,U238_ng,Emesh)[-1])
+#ax.loglog(Emesh, 1/Emesh*Emesh[-1])
+#plt.xlabel('Energy [eV]'), plt.ylabel('$\phi(E)$')
+#
+#
+#Nk = [100.0, 1.0]
+#Ak = [alpha(1), alpha(238)]
+#St = 1.0*(U238_tot)+100.0*H1_es
+#Ss = [H1_es, U238_es]
+#
+#phi = compute_spectrum(Emesh, St, Ss, Nk, Ak)  
+#ax.loglog(Emesh, phi/phi[0]*max(1/Emesh*Emesh[-1]))
+#plt.legend(['$\phi_{nr}$','$\phi_{wr}$','1/E', 'Slowing Down $\phi$'])
+#plt.tight_layout()
