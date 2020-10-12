27dedfcb8bb0ebebff7ade48d9c3ecde50f60284
Keith <kdhuddle@ksu.edu>
2020-10-12 13:49:09 -0500

    Commenting Code

--- HW3/Main.py
+++ HW3/Main.py
@@ -30,61 +30,89 @@ H3 = lambda a, b: F3(a) - F3(b)
 F4 = lambda a: (3/4)*erf(a) - np.sqrt(1/np.pi)*((3*a/2)+a**3)*np.exp(-a**2)
 H4 = lambda a, b: F4(a) - F4(b)
 
-
 def Akf(E1, E2, S1, S2):
     den = (E2 - E1)
     num = (E2*S1) - (E1*S2)
     return num/den
-
 
 def Ckf(E1, E2, S1, S2, alpha):
     den = (E2 - E1)*alpha
     num = (S2 - S1)
     return num/den
 
+def doppler(E2, E1, S1, T1, T2, M, m=1.009,  neg=False):
+    """
 
-def doppler(E2, E1, S1, T1, T2, m, M, neg=False):
-    Bk = 6.617*10**-5  # ev K^-1
-    alpha = (M/m)/(Bk*(T2-T1))
-    S2 = np.zeros(len(E2))
+    Parameters
+    ----------
+    E2 : array_like
+        New energy mesh at which to evaluate cross sections at.
+    E1 : array_like
+        Energy mesh of the reference cross section data.
+    S1 : array_like
+        Cross section data of reference.
+    T1 : Float
+        Temperature of the reference cross section data.
+    T2 : Float
+        New temperature at which to evaluate cross sections at.
+    m : Float
+        Atomic mass of projectile, 1.009 for Neutron.
+    M : Float
+        Atomic mass of target.
+    neg : Int, optional
+        DESCRIPTION. The default is False.
+
+    Returns
+    -------
+    S2 : array_like
+        Reavaluated cross section data for energies E2, and temperature T2
+
+    """
+    Bk = 6.617*10**-5           # Boltzman Constant, [ev K^-1]
+    alpha = (M/m)/(Bk*(T2-T1))  # Alpha term found in Doppler broadening Eqs.
+    S2 = np.zeros(len(E2))      # Initialize new cross section data array
+    S2_pos = np.zeros(len(E2)) 
+    S2_neg = np.zeros(len(E2)) 
+    # Evaluate Doppler-broadened cross section at specified energy E2[i]
     for i in range(len(E2)):
         S2i = 0
+
         E2i = E2[i]
         y = np.sqrt(alpha*E2i)
-        if not neg:
-            pass
-        else:
-            y = -1*y
-        if True:
-        # for k in range(len(E1)-1):
+        y = [-1*y, 1*y]
+        for j in range(len(y)):
             Ek1 = E1[:-1]
-            xk1 = np.sqrt(alpha*Ek1)
-
             Ek2 = E1[1:]
+            
+            xk1 = np.sqrt(alpha*Ek1)
             xk2 = np.sqrt(alpha*Ek2)
-
+    
             Sk1 = S1[:-1]
             Sk2 = S1[1:]
-
+    
             Ak = Akf(Ek1, Ek2, Sk1, Sk2)
             Ck = Ckf(Ek1, Ek2, Sk1, Sk2, alpha)
-
-            Zk1 = xk1 - y
-            Zk2 = xk2 - y
-
+    
+            Zk1 = xk1 - y[j]
+            Zk2 = xk2 - y[j]
+    
             H0k = H0(Zk2, Zk1)
             H1k = H1(Zk2, Zk1)
             H2k = H2(Zk2, Zk1)
             H3k = H3(Zk2, Zk1)
             H4k = H4(Zk2, Zk1)
-
+    
             S2i = H4k * (Ck) \
-                 + H3k * (4*Ck*y) \
-                 + H2k * (Ak+6*Ck*y**2) \
-                 + H1k * (2*Ak*y+4*Ck*y**3) \
-                 + H0k * (Ak*y**2+Ck*y**4)
-        S2i = sum(S2i)
-        S2[i] = S2i/2/y**2
+                 + H3k * (4*Ck*y[j]) \
+                 + H2k * (Ak+6*Ck*y[j]**2) \
+                 + H1k * (2*Ak*y[j]+4*Ck*y[j]**3) \
+                 + H0k * (Ak*y[j]**2+Ck*y[j]**4)
+            S2i = sum(S2i)
+            if j == 0:
+                S2_neg[i] = S2i/2/y[j]**2
+            else:
+                S2_pos[i] = S2i/2/y[j]**2
+            S2 = S2_pos - S2_neg
     return S2
 
 # ============================================================================
@@ -155,6 +183,7 @@ def breitWigner(E0, J, GN, GG, GFA, GFB, A, sigma_p, I, E):
 
     sigma_e = sigma_e + sigma_n  # Add potential scattering
     return sigma_e, sigma_g
+
 def fwxs(E, X, Phi, GS):
     """
     Input:
@@ -283,8 +312,7 @@ M = 1.008
 Emesh = H1i.ESEM['300']
 E = H1i.ESEM['300']
 XS = H1i.ESXS['300']
-H1i.ESXS['600'] = doppler(Emesh, E, XS, 300, 600, m, M) - \
-                  doppler(Emesh, E, XS, 300, 600, m, M, True)
+H1i.ESXS['600'] = doppler(Emesh, E, XS, 300, 600, m, M)
 H1i.ESEM['600'] = E
 for key in H1i.ESEM.keys():
     print(key)
